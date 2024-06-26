6eb0b94cb7bf2e586b4c8b55814ec9ee01d5236f
FoititikosOdigosDit <hahaslolys@gmail.com>
2024-06-26 23:33:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+// Function to open the popup
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// Open the popup when the page loads
+window.onload = function() {
+    openPopup();
+};
